93113357c9728db2f2432310658ad8b8c7589e5b
Milan Vuksan <milan.vuksan@ems.rs>
2021-09-13 10:17:01 +0200

    posts

---
+++ src/posts.js
@@ -0,0 +1,45 @@
+import * as React from "react";
+import {
+  Datagrid,
+  TextField,
+  ReferenceField,
+  EditButton,
+  Edit,
+  Create,
+  SimpleForm,
+  ReferenceInput,
+  SelectInput,
+  TextInput,
+  List,
+  SimpleList,
+} from "react-admin";
+
+const postFilters = [
+  <TextInput source="q" label="Search" alwaysOn />,
+  <ReferenceInput source="userId" label="User" reference="users" allowEmpty>
+    <SelectInput optionText="name" />
+  </ReferenceInput>,
+];
+
+export const PostList = (props) => (
+  <List filters={postFilters} {...props}></List>
+);
+
+const PostTitle = ({ record }) => {
+  return <span>Post {record ? `"${record.title}"` : ""}</span>;
+};
+
+export const PostEdit = (props) => (
+  <Edit title={<PostTitle />} {...props}></Edit>
+);
+export const PostCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
+  </Create>
+);
