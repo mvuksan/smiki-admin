e0080a0e57151f3c69659a7d43dba444fd01dac0
Milan Vuksan <milan.vuksan@ems.rs>
2021-09-13 10:23:49 +0200

    fin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import * as React from "react";
 
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource } from "react-admin";
+import { PostList, PostEdit, PostCreate } from "./posts";
 import jsonServerProvider from "ra-data-json-server";
+import { UserList } from "./users";
+
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
+
+import Dashboard from "./Dashboard";
+import authProvider from "./authProvider";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+  <Admin
+    dashboard={Dashboard}
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+  >
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      icon={PostIcon}
+    />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 );
 
---
+++ src/users.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { List, Datagrid, TextField, EmailField } from "react-admin";
+import MyUrlField from "./MyUrlField";
+
+export const UserList = (props) => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <TextField source="name" />
+
+      <EmailField source="email" />
+
+      <MyUrlField source="website" />
+    </Datagrid>
+  </List>
+);
